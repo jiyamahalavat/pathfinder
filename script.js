ab70de097a55b3391ca4a785abf8a004f809ef3a
jiyamahalavat <jiyamahalavat@gmail.com>
2026-04-15 13:10:51 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+class Cell{
+    //blueprint for all cells since there are so many 
+}
+
+class Grid {
+    //shows the array of cells
+    // functions like createGrid() & getNeighbors()
+
+}
+
+class Pathfinder {
+    //handles the algorithms
+    // functions like 
+}
+
+class UiController {
+    //focuses on the User Interface
+    // functions like resetGrid(), handClick() -> what to do if user clicks button
+    // and startAlgorithm() ->kickstarts the algorithm and all its features
+}
